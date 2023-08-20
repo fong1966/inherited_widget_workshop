121b59a8874b7d064d21aa19be80cb5588e4681a
fong1966 <f1966kk@gmail.com>
2023-08-20 22:57:51 +0800

    refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-import 'cart.dart';
+import 'shopping_cart.dart';
 import 'data.dart';
 
-import 'product.dart';
+import 'product_list.dart';
 import 'service.dart';
 
 final GlobalKey<ShoppingCartIconState> shoppingCart =
@@ -92,59 +92,6 @@ class MyStorePageState extends State<MyStorePage> {
           ),
         ],
       ),
-    );
-  }
-}
-
-class ProductListWidget extends StatefulWidget {
-  const ProductListWidget({Key? key}) : super(key: key);
-
-  @override
-  ProductListWidgetState createState() => ProductListWidgetState();
-}
-
-class ProductListWidgetState extends State<ProductListWidget> {
-  List<String> get productList => _productList;
-  List<String> _productList = Server.getProductList();
-
-  set productList(List<String> value) {
-    setState(() {
-      _productList = value;
-    });
-  }
-
-  Set<String> get itemsInCart => _itemsInCart;
-  Set<String> _itemsInCart = <String>{};
-
-  set itemsInCart(Set<String> value) {
-    setState(() {
-      _itemsInCart = value;
-    });
-  }
-
-  void _handleAddToCart(String id) {
-    itemsInCart = _itemsInCart..add(id);
-    shoppingCart.currentState!.itemsInCart = itemsInCart;
-  }
-
-  void _handleRemoveFromCart(String id) {
-    itemsInCart = _itemsInCart..remove(id);
-    shoppingCart.currentState!.itemsInCart = itemsInCart;
-  }
-
-  Widget _buildProductTile(String id) {
-    return ProductTile(
-      product: Server.getProductById(id),
-      purchased: itemsInCart.contains(id),
-      onAddToCart: () => _handleAddToCart(id),
-      onRemoveFromCart: () => _handleRemoveFromCart(id),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: productList.map(_buildProductTile).toList(),
     );
   }
 }
